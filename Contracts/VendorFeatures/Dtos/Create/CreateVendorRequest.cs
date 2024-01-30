51dcc4e77ed33777a26db3cb8bec4a8c112de340
asseemy <asseemy@enoc.com>
2024-01-30 16:16:19 +0400

    ##

--- Contracts/VendorFeatures/Dtos/Create/CreateVendorRequest.cs
+++ Contracts/VendorFeatures/Dtos/Create/CreateVendorRequest.cs
@@ -4,18 +4,8 @@ public class CreateVendorRequest
 {
     public string Id { get; set; } = default!;
     public string Email { get; set; } = default!;
-    
     public string Name { get; set; } = default!;
-    
     public string FirstSearchTerm { get; set; } = default!;
-    
     public string Language { get; set; } = default!;
     public string Phone { get; set; } = default!;
-    public Test Test { get; set; } = default!;
-
-}
-
-public class Test
-{
-    public string Email { get; set; } = default!;
 }
